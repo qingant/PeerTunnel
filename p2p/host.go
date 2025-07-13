7e4bc49f96b3d5ae7ffa716d6bd44215d4c2f8c5
Tao <qingant@gmail.com>
2025-07-14 01:10:59 +0800

    fix: manually configure DHT to fix bootstrap issue

--- p2p/host.go
+++ p2p/host.go
@@ -8,13 +8,15 @@ import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/routing"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/p2p/security/noise"
 	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
 	ws "github.com/libp2p/go-libp2p/p2p/transport/websocket"
 	"github.com/multiformats/go-multiaddr"
 )
 
-var defaultRelays = []string{
+var defaultBootstrapPeers = []string{
 	"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
 	"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
 	"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
@@ -24,25 +26,26 @@ var defaultRelays = []string{
 
 // NewHost creates a new libp2p host.
 func NewHost(ctx context.Context, relays string) (host.Host, error) {
-	// Combine default and custom relays
-	allRelayAddrs := defaultRelays
+	// Combine default and custom bootstrap peers
+	allBootstrapAddrs := defaultBootstrapPeers
 	if relays != "" {
-		allRelayAddrs = append(allRelayAddrs, strings.Split(relays, ",")...)
+		allBootstrapAddrs = append(allBootstrapAddrs, strings.Split(relays, ",")...)
 	}
 
-	var relayAddrInfos []peer.AddrInfo
-	for _, rAddr := range allRelayAddrs {
-		maddr, err := multiaddr.NewMultiaddr(rAddr)
+	var bootstrapPeers []peer.AddrInfo
+	for _, pAddr := range allBootstrapAddrs {
+		maddr, err := multiaddr.NewMultiaddr(pAddr)
 		if err != nil {
-			return nil, fmt.Errorf("invalid relay address %s: %w", rAddr, err)
+			return nil, fmt.Errorf("invalid bootstrap peer address %s: %w", pAddr, err)
 		}
 		addrInfo, err := peer.AddrInfoFromP2pAddr(maddr)
 		if err != nil {
 			return nil, fmt.Errorf("failed to create AddrInfo from multiaddress: %w", err)
 		}
-		relayAddrInfos = append(relayAddrInfos, *addrInfo)
+		bootstrapPeers = append(bootstrapPeers, *addrInfo)
 	}
 
+	var newDHT routing.PeerRouting
 	opts := []libp2p.Option{
 		libp2p.ListenAddrStrings(
 			"/ip4/0.0.0.0/tcp/0", // Listen on all IPv4 addresses
@@ -52,8 +55,13 @@ func NewHost(ctx context.Context, relays string) (host.Host, error) {
 		libp2p.Transport(tcp.NewTCPTransport),
 		libp2p.EnableRelay(),
 		libp2p.EnableAutoNATv2(),
-		libp2p.EnableAutoRelayWithStaticRelays(relayAddrInfos),
+		libp2p.EnableAutoRelay(),
 		libp2p.Security(noise.ID, noise.New),
+		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
+			var err error
+			newDHT, err = dht.New(ctx, h, dht.BootstrapPeers(bootstrapPeers...))
+			return newDHT, err
+		}),
 	}
 
 	h, err := libp2p.New(opts...)
